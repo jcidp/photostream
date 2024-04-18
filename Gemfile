9ed9c58c50035e9aa34216dafa3ce2a76f247fe2
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-04-17 21:09:24 -0600

    Add Figaro and use own credentials

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ group :test do
 end
 
 gem "rest-client"
+
+gem "figaro"
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,4 @@
 require "json"
-require "rest-client"
 
 class StaticPagesController < ApplicationController
   def view
@@ -10,7 +9,7 @@ class StaticPagesController < ApplicationController
 
   def photos(user_id)
     return [] unless user_id
-    response = RestClient.get("https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=c1c58b20755be4eb94b18f5d5a25310b&user_id=#{user_id}&format=json&nojsoncallback=1&api_sig=4ffca29d06f2d28e117847e4b02e473a", accept: :json)
+    response = RestClient.get("https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=#{ENV["flickr_api_key"]}&user_id=#{user_id}&format=json&nojsoncallback=1", accept: :json)
     owner_photos = JSON.parse(response.body).fetch("photos", {}).fetch("photo", {})
   end
 end
